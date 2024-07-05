316adce0fba8cfbbb46c9063d996f434f3bc2224
RedSTARO <822858602@qq.com>
2024-07-05 19:10:41 +0800

    Add tqdm progress hint

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from tools import *
 import json
 import os
 import re
+from tqdm import tqdm
 
-accurateMode = False
+accurateMode = True
 
 failCount = 0
-data = json.loads(getSongs(input("Please paste the link of your song list here"))
+songListURL = input("Please paste the link of your song list here:")
+
+if songListURL == "":
+    songListURL = "https://music.163.com/#/playlist?id=3219642517"
+
+data = json.loads(getSongs(songListURL))
 
 print("Please open the installation directory of Beat Saber")
 gameDir = os.path.join(selectDir(), "Beat Saber_Data/CustomLevels")
@@ -20,32 +26,32 @@ else:
     print(f"Directory '{gameDir}' has been found")
 
 
-print("Fetch Status:", data['msg'])
+print("Playlist fetch Status:", data['msg'])
 print("Playlist Name:", data['data']['name'])
 print("Songs Count:", data['data']['songs_count'])
 print("Songs List:")
-for song in data['data']['songs']:
+for song in tqdm(data['data']['songs'], desc = "Progress", unit = "Songs"):
     songName = song.split(" - ")[0]
     if accurateMode == True:
         song = songName
-    print("Fetching " + song)
+    tqdm.write("Fetching " + song)
     mapResult = getMap(song)
     if mapResult == "E1":
-        print(f"No map for {song}")
+        tqdm.write(f"No map for {song}")
         failCount += 1
     else:
         # print("URL:" + mapResult)
         downloadPath = os.path.join(gameDir, re.sub(r'[\\/*?:"<>|]', '', songName), "data.zip")
         downloadResult = downloader(mapResult, downloadPath)
         if downloadResult == "E2":
-            print(f"Error when download {gameDir} to {songName}.zip")
+            tqdm.write(f"Error when download {gameDir} to {songName}.zip")
             failCount += 1
         else:
             # unzip downloaded stuff
             unzipStatus = unzip(downloadPath)
-            print(downloadPath)
+            tqdm.write("Unziping " + downloadPath)
             if unzipStatus == "E3":
-                print(f"Error when unzip {downloadPath}")
+                tqdm.write(f"Error when unzip {downloadPath}")
                 failCount += 1
 
 print("Songs Count:", data['data']['songs_count'])
